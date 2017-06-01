8cbf83ff8417413e009eeb0897cf8cf4bb2a44f4
Masha Zorin <masha.zorin@gmail.com>
2017-06-01 12:30:25 -0400

    wrote constructor

--- Map.java
+++ Map.java
@@ -1,2 +1,30 @@
+import java.io.*;
+import java.util.*;
 public class Map {
+    private ArrayList<Node> setOfPoints;
+    private double sizeX;
+    private double sizeY;
+
+    public Map(String filename){
+	setOfPoints = new ArrayList<Node>();
+	try{
+	    File file = new File(filename);
+	    Scanner scan = new Scanner(file);
+	    sizeX = scan.nextDouble();
+	    sizeY = scan.nextDouble();
+	    double x, y;
+	    while(scan.hasNextDouble()){
+		x = scan.nextDouble();
+		y = scan.nextDouble();
+		Node point = new Node (x,y);
+		setOfPoints.add(point);
+	    }
+	}
+	catch(FileNotFoundException e){
+	}
+    }
+
+    public String toString(){
+	//stuff
+    }
 }

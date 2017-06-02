1b7740ce7025cbb05f2122bb730e7648ad7fed1b
jt1250champ <jtao@stuy.edu>
2017-06-02 12:32:45 -0400

    sdhfsd

--- SmallestIncrease.java
+++ SmallestIncrease.java
@@ -1,3 +1,10 @@
+import java.util.*;
+import java.awt.*;
+import java.awt.geom.*;
+
 public class SmallestIncrease implements Solver {
+    private Map map;
+    private LinkedList<Node> solution;
+    
     
 }

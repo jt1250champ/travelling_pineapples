b9d9df5bb8ed4e87659299f902a519c2c454e7b9
jt1250champ <jtao@stuy.edu>
2017-06-04 19:35:16 -0400

    eh didnt do much

--- SmallestIncrease.java
+++ SmallestIncrease.java
@@ -5,6 +5,23 @@ import java.awt.geom.*;
 public class SmallestIncrease implements Solver {
     private Map map;
     private LinkedList<Node> solution;
+    private double dist;
+
+    public void solve() {
+	
+    }
+
+    public void solve(Node start) {
+    }
+
+    public double getDist() {
+	return dist;
+    }
+
+    public String toString() {
+	return "Dist: " + this.getDist() + "\n" + "The path taken is ";
+	//TODO uh i have to do the thing wtih the path but that should be in my linked list so im gonna have to find that and then...do the thing this comment makes no sense sorry
+    }
     
     
 }

9fe2fe58969c0252dfd355499968098fb8b0a3b0
Masha Zorin <masha.zorin@gmail.com>
2017-06-04 22:19:29 -0400

    wrote constructor

--- NearestNeighbor.java
+++ NearestNeighbor.java
@@ -1,2 +1,15 @@
 public class NearestNeighbor implements Solver {
+    private Map map;
+    private LinkedList<Node> solution;
+
+    public NearestNeighbor(Map file){
+	map = file;
+	solution = new LinkedList<Node>();
+    }
+    
+    public void solve(){
+    }
+
+    public void solve(Node pine){
+    }
 }

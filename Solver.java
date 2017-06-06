05cef82ed799cd2b299c9eee5c02fbad798e4d25
Masha Zorin <masha.zorin@gmail.com>
2017-06-06 12:35:59 -0400

    wrote something for toString

--- Solver.java
+++ Solver.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public abstract class Solver {
     private Map map;
-    private LinkedList<Node> solution;
+    private ArrayList<Node> solution;
     private double dist;
     
     abstract void solve();
@@ -12,7 +12,11 @@ public abstract class Solver {
     }
     
     public String toString() {
-	return "Dist: " + dist + "\n" + "The path taken is ";
+	String nodes = "";
+	for (i = 0; i < solution.size(); i ++){
+	    nodes += solution.get(i) + "\n";
+	}
+	return "Dist: " + dist + "\n" + "The path taken is: \n" + nodes;
 	//TODO uh i have to do the thing wtih the path but that should be in my linked list so im gonna have to find that and then...do the thing this comment makes no sense sorry
     }
     

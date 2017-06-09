71c1f32388187883e5520537154a52b58825d09f
Masha Zorin <masha.zorin@gmail.com>
2017-06-09 12:31:57 -0400

    started writing stuff for ConvexHull

--- ConvexHull.java
+++ ConvexHull.java
@@ -2,11 +2,13 @@ import java.util.*;
 public class ConvexHull extends Solver {
     private Map map;
     private ArrayList<Node> solution;
+    private ConvexPolygon polygon;
     private double dist;
     
     public ConvexHull(Map file){
 	map = file;
 	solution = new ArrayList<Node>();
+	polygon = new ConvexPolygon();
 	dist = 0;
     }
 
@@ -14,6 +16,16 @@ public class ConvexHull extends Solver {
     }
 
     public ArrayList<Node> findOuterPolygon(){
+	Node node;
+	for (i = 0; i < 3; i ++){
+	    node = map.findOuterNode();
+	    polygon.add(node);
+	    node.setPassed(true);
+	}
+    }
+
+    public void insert(Node pine){
+	
     }
     
 }

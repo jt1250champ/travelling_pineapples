32b7d21eee3542104009b399ad8f65b3cf77b54e
Masha Zorin <masha.zorin@gmail.com>
2017-06-10 12:09:40 -0400

    Figured out methods needed and wrote findOuterPolygon

--- ConvexHull.java
+++ ConvexHull.java
@@ -2,30 +2,43 @@ import java.util.*;
 public class ConvexHull extends Solver {
     private Map map;
     private ArrayList<Node> solution;
-    private ConvexPolygon polygon;
     private double dist;
     
     public ConvexHull(Map file){
 	map = file;
 	solution = new ArrayList<Node>();
-	polygon = new ConvexPolygon();
 	dist = 0;
     }
 
     public void solve(){
+	findOuterPolygon();
+	while(someNodesNotPassed()){ //copy from NearestNeighbor
+	    insert(findNodeNearestToPolygon());
+	}
     }
 
-    public ArrayList<Node> findOuterPolygon(){
+    public void findOuterPolygon(){
 	Node node;
 	for (i = 0; i < 3; i ++){
+	    node = map.findOuterNode();//make method in Map
+	    solution.add(node);
+	    node.setPassed(true);
+	}
+	while (areMoreOuterNodes()){
 	    node = map.findOuterNode();
-	    polygon.add(node);
+	    insert(node);
 	    node.setPassed(true);
 	}
     }
 
     public void insert(Node pine){
 	
+    }
+
+    public boolean areMoreOuterNodes(){
+    }
+
+    public Node findNodeNearestToPolygon(){
     }
     
 }

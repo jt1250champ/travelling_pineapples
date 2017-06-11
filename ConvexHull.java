33e0d71178fc33b075c7ce46f827a7a2edcafb0f
Masha Zorin <masha.zorin@gmail.com>
2017-06-11 12:29:30 -0400

    almost done writing methods

--- ConvexHull.java
+++ ConvexHull.java
@@ -20,18 +20,19 @@ public class ConvexHull extends Solver {
     public void findOuterPolygon(){
 	Node node;
 	for (i = 0; i < 3; i ++){
-	    node = map.findOuterNode();//make method in Map
+	    node = findOuterNode();
 	    solution.add(node);
 	    node.setPassed(true);
 	}
 	while (areMoreOuterNodes()){
-	    node = map.findOuterNode();
+	    node = findOuterNode();
 	    insert(node);
 	    node.setPassed(true);
 	}
     }
 
-    public void insert(Node pine){
+    //return index of first node in solution
+    public int findNearestEdge(Node pine){
 	int nodeIndex;
 	double leastDist = map.getDiagonalSize();
 	Edge edge;
@@ -42,10 +43,56 @@ public class ConvexHull extends Solver {
 		nodeIndex = i;
 	    }
 	}
-	solution.add(i, pine);
+	return nodeIndex;
+    }
+
+    public void insert(Node pine){
+	int nodeIndex = findNearestEdge(pine) + 1;
+	solution.add(nodeIndex, pine);
     }
 
     public boolean areMoreOuterNodes(){
+	Node node;
+	Edge pine, apples;
+	for (int i = 0; i < map.getPoints().size(); i ++){
+	    node = map.getPoints().get(i);
+	    pine = new Edge(node, new Node(map.getSizeX()/2, map.getSizeY()/2));
+	    for (int j = 0; j < solution.size(); j ++){
+		apples = new Edge(solution.get(j), solution.get((j + 1) % solution.size()));
+		if (pine.intersect(apples)){
+		    return true;
+		}
+	    }
+	}
+	return false;
+    }
+
+    public Node findOuterNode(){
+	double leastDist = map.getDiagonalSize();
+	Node node;
+	Node answer;
+	for (int i = 0; i < map.getPoints().size(); i ++){
+	    node = map.getPoints().get(i);
+	    if (!node.getPassed()){
+		if (node.getX() < leastDist){
+		    leastDist = node.getX();
+		    answer = node;
+		}
+		if (node.getY() < leastDist){
+		    leastDist = node.getY();
+		    answer = node;
+		}
+		if (map.sizeX() - node.getX() < leastDist){
+		    leastDist = map.sizeX() - node.getX();
+		    answer = node;
+		}
+		if (map.sizeY() - node.getY() < leastDist){
+		    leastDist = map.sizeY() - node.getY();
+		    answer = node;
+		}
+	    }
+	}
+	return answer;
     }
 
     public Node findNodeNearestToPolygon(){
@@ -58,8 +105,8 @@ public class ConvexHull extends Solver {
 		answer = node;
 		Edge edge;
 		double dist;
-		for (int i = 0; i < solution.size() - 1; i ++){
-		    edge = new Edge(solution.get(i), solution.get(i + 1));
+		for (int j = 0; j < solution.size() - 1; j ++){
+		    edge = new Edge(solution.get(j), solution.get(j + 1));
 		    dist = node.distToEdge(edge); //write this in node
 		    if (dist < leastDist){
 			leastDist = dist;
@@ -70,5 +117,13 @@ public class ConvexHull extends Solver {
 	}
 	return answer;
     }
-    
+
+    public boolean someNodesNotPassed(){
+	for (int i = 0; i < map.getPoints().size(); i ++){
+	    if (!map.getPoints().get(i).getPassed()){
+		return true;
+	    }
+	}
+	return false;
+    }
 }

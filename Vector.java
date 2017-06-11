64391e8688715f137541be63b3c29a9fe2db842c
Masha Zorin <masha.zorin@gmail.com>
2017-06-11 19:17:37 -0400

    wrote Vector

--- Vector.java
+++ Vector.java
@@ -1,37 +1,74 @@
+import java.util.*;
 public class Vector{
-    private Node endNode;
+    private double x;
+    private double y;
     private double magnitude;
+    private double direction;
 
     public Vector(Node pine, Node apple){
-	endNode = new Node(pine.getX() - apple.getX(), pine.getY() - apple.getX());
-	magnitude = Math.sqrt(endNode.getX() * endNode.getX() +
-			      endNode.getY() * endNode.getY());
+	x = apple.getX() - pine.getX();
+	y = apple.getY() - pine.getY();
+	magnitude = Math.sqrt(x * x + y * y);
+	direction = Math.asin(y / x);
     }
 
     public Vector(Edge edge){
 	this(edge.pine, edge.apple);
     }
 
-    public Node getEndNode(){
-	return endNode;
+    public Vector(double dir, double mag){
+	direction = dir;
+	magnitude = mag;
+	x = mag * Math.cos(dir);
+	y = mag * Math.sin(dir);
+    }
+
+    public Vector(double X, double Y){
+	x = X;
+	y = Y;
+	magnitude = Math.sqrt(x * x + y * y);
+	direction = Math.asin(y / x);
+    }
+
+    public double getX(){
+	return x;
+    }
+
+    public double getY(){
+	return y;
     }
 
     public double getMagnitude(){
 	return magnitude;
     }
 
+    public double getDirection(){
+	return direction;
+    }
+
+    public void setMagnitude(double mag){
+	x = x / magnitude * mag;
+	y = y / magnitude * mag;
+	magnitude = mag;
+    }
+
     public Vector getNormal(){
+	return new Vector(direction + Math.pi / 2, 1);
     }
 
     public double dot(Vector other){
+	return x * other.getX() + y * other.getY();
     }
 
     public Vector scale(double scalor){
+	return new Vector(direction, magnitude * scale);
     }
 
     public Vector add(Vector other){
+	return new Vector(x + other.getX(), y + other.getY());
     }
 
     public Vector minus(Vector other){
+	return new Vector(x - other.getX(), y - other.getY());
     }
 }
